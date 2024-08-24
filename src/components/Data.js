05a4cbd833cd6638ac4c088694525b06e3bb6f41
Badrinarayan2003 <badrithamba@gmail.com>
2024-08-24 21:55:10 +0530

    add project section

---
+++ src/components/Data.js
@@ -0,0 +1,62 @@
+export const projectData = [
+    {
+        p_id: 1,
+        p_name: "Ez Clothing",
+        p_type: "(eCommerce Website - Frontend Only)",
+        p_desc: "I built an eCommerce shopping website using React.js and integrated products through the Amazon API from RapidAPI. This project helped me deepen my React skills and introduced me to Redux for best state management practices.",
+        p_tech: ["react.js", "redux", "html", "css", "bootstrap", "jsx", "vercel", "smtpjs", "github"],
+        color: "#008db6",
+        image: "./assets/images/project-1.png",
+        row_direction: 'row',
+        position: "",
+        p_url: "https://ez-pizi.vercel.app/"
+    },
+    {
+        p_id: 2,
+        p_name: "Portfolio",
+        p_type: "(Personal Portfolio)",
+        p_desc: "I developed my personal portfolio with React.js to showcase my projects, highlight my web development expertise, and provide an organized view of my skills and experience.",
+        p_tech: ["react.js", "html", "css", "bootstrap", "jsx", "netlify", "smtpjs", "github"],
+        color: "rgba(236, 162, 31, 1)",
+        image: "./assets/images/project-2.png",
+        row_direction: 'row-reverse',
+        position: "justify-content-end",
+        p_url: "https://badrinarayan.netlify.app/"
+    },
+    {
+        p_id: 3,
+        p_name: "NexGen Studios",
+        p_type: "(Digital Marketing Website - Using Wordpress)",
+        p_desc: "I worked alongside a friend to create a digital marketing website using WordPress, contributing to the design and development of the site. I also ensured the site is fully optimized for both mobile and tablet devices.",
+        p_tech: ["Wordpress","elementor"],
+        color: "#1ec2e3",
+        image: "./assets/images/project-3.png",
+        row_direction: 'row',
+        position: "",
+        p_url: "https://nexgenstudios.in/"
+    },
+    {
+        p_id: 4,
+        p_name: "Weather Web App",
+        p_type: "(Weather App)",
+        p_desc: "I developed this application during the early stages of learning React. It uses an external API to fetch live weather data and features a user-friendly weather app with a well-designed interface.",
+        p_tech: ["react.js", "html", "css", "bootstrap", "jsx", "vercel", "openweatherAPI"],
+        color: "#e85e1e",
+        image: "./assets/images/project-4.png",
+        row_direction: 'row-reverse',
+        position: "justify-content-end",
+        p_url: "https://weather-app-badri-narayans-projects.vercel.app/"
+    },
+    {
+        p_id: 5,
+        p_name: "Older Portfolio",
+        p_type: "(My personal older Portfolio)",
+        p_desc: "I also have an older portfolio developed using the same technology, showcasing earlier projects and skills in web development.",
+        p_tech: ["react.js", "html", "css", "bootstrap", "jsx", "vercel"],
+        color: "#39adbd",
+        image: "./assets/images/project-5.png",
+        row_direction: 'row',
+        position: "",
+        p_url: "https://badri-visual-vault.vercel.app/"
+    },
+]
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,65 @@
+import { projectData } from "./Data.js"
+import { useState, useEffect } from "react";
+import { TbExternalLink } from "react-icons/tb";
+
 function Project() {
+    const [center, setCenter] = useState(false)
+
+    const checkScreenSize = () => {
+        if (window.innerWidth > 767) {
+            setCenter(true);
+        } else {
+            setCenter(false);
+        }
+    };
+
+    useEffect(() => {
+        checkScreenSize();
+        window.addEventListener('resize', checkScreenSize);
+
+        // Cleanup event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', checkScreenSize);
+        };
+
+    }, []);
+
+
     return (
         <div className="project-section overflow-hidden">
-            desgcdxbtr
+            <h2 className="text-center fw-bold project-heading"><span>My Latest Works</span></h2>
+            {
+                projectData.map((item) => {
+                    return (
+                        <div className="row" key={item.p_id} style={{ flexDirection: item.row_direction }}>
+                            <div className={`col-md-5 d-flex align-items-center project-img-parent-box ${center && item.position}`}>
+                                <div className={`project-img-box ${center && `${item.position ? "me-4" : "ms-4"}`}`}>
+                                    <img src={item.image} alt={`image-${item.p_id}`} />
+                                </div>
+                            </div>
+                            <div className="col-md-2"></div>
+                            <div className="col-md-5 d-flex align-items-center project-content-parent-box">
+                                <div className="project-content-box">
+                                    <h4 className="fw-bold project-centent-heading"><a href={item.p_url} style={{ color: item.color }}>{item.p_name} <TbExternalLink /></a></h4>
+                                    <p className="mb-2 project-centent-sub-heading" style={{ color: item.color }}>{item.p_type}</p>
+                                    <p className="project-content-description">{item.p_desc}</p>
+                                    <div className="project-used-technology-box d-flex">
+                                        {
+                                            item && item.p_tech.map((tech, index) => {
+                                                return (
+                                                    <p key={index} className="mb-2 me-2 project-used-technology-text">#{tech}</p>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
+
 export default Project;
